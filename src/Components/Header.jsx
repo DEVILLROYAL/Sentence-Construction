e24f4366795c800d015d2cfdf60483045497adbf
DEVILLROYAL <deepakyadav4014@gmail.com>
2025-04-14 05:20:44 +0530

    developed the overall font-end structure

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 
-
-
 export default function Header() {
 
   const [data, setData] = useState([]);
-  const [count , setCount] = useState();
+  const [count , setCount] = useState(0);
+  const [pageCount , setPageCount] = useState(1);
+  const [over, setOver] = useState(30);
+
 
   useEffect(() => {
     fetch("http://localhost:3001/data")
@@ -14,13 +15,31 @@ export default function Header() {
       .then((data) => setData(data.questions))
       .catch((error) => console.error("Error fetching data:", error));
   }, []); 
-  
 
-  const questionCount = () => {
-  }
-  const nextClick = () => {
+
+  useEffect(() => {
+    if (over > 0) {
+      const timer = setInterval(() => {
+        setOver((prevCount) => prevCount - 1 );
+      }, 1000 );
+      return () => clearInterval(timer);
+    }else if (over == 0){
+      handleNextClick();
+    }
+  [over]});
+
+  const handleNextClick = () => {
+    setCount(count + 1);
+    setPageCount(pageCount + 1);
+    setOver(30);
   };
+ 
+  const first = document.getElementById('option-1');
+  const second = document.getElementById('option-2');
+  const third = document.getElementById('option-3');
+  const fourth = document.getElementById('option-4');
 
+  const options = [  ];
 
   return (
     <>  
@@ -30,22 +49,25 @@ export default function Header() {
               </div>
               <div className=" border-2 flex flex-col p-5 gap-5 ">
                 <div className="flex justify-center relative ">
-                  <p className=" font-extralight absolute left-0 " id="timer" >{`${count} : Seconds left`}</p>
-                <h1 className=" font-bold m-auto" >{`${count}`}</h1>
+                  <p className=" font-extralight absolute left-0 " id="timer" >{`${over} : Seconds left`}</p>
+                <h1 className=" font-bold m-auto" >Question no.{`${pageCount}`}</h1>
                 </div>
-                <p id="question" className=" text-left" >{data[`${0}`]?.question}</p>
+                <p id="question" className=" text-left" >{data[`${count}`]?.question}</p>
                 </div>
                 <div className="h-auto border-2 ">
                   <div id="options" className=" p-5 flex justify-evenly " >
-                    <button className="border-1 p-1 rounded-xs " >{data[0]?.options[0]}</button>
-                    <button>{data[0]?.options[1]}</button>
-                    <button>{data[0]?.options[2]}</button>
-                    <button>{data[0]?.options[3]}</button>
+                    <button className="border-1 p-1 rounded-xs" id="option-1" >{data[`${count}`]?.options[0]}</button>
+                    <button className="border-1 p-1 rounded-xs" id="option-2" >{data[`${count}`]?.options[1]}</button>
+                    <button className="border-1 p-1 rounded-xs" id="option-3" >{data[`${count}`]?.options[2]}</button>
+                    <button className="border-1 p-1 rounded-xs" id="option-4" >{data[`${count}`]?.options[3]}</button>
                   </div>
                 </div>
-                <div className=" flex justify-between ">
-                  <button className="hover:bg-gray-200 font-bold py-2 px-4 rounded-l-3xl" onClick={nextClick} >Prev</button>
-                  <button className="hover:bg-gray-200 font-bold py-2 px-4 rounded-r-3xl" >Next</button>
+                <div className=" flex justify-end ">
+                  {count < 9 ? (
+                     <button className="hover:bg-gray-200 font-bold py-2 px-4 rounded-r-3xl" id="nextButton" onClick={handleNextClick} >Next</button>
+                      ) : (
+                         <button className="hover:bg-gray-200 font-bold py-2 px-4 rounded-r-3xl" > Submit </button>
+                          ) }
                   </div>
                   </div>      
                   
